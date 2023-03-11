715d82c1ddefde0161a46fea2a279bcb9f8f220f
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2023-03-11 22:21:11 +0200

    test fibonacci clock component

--- src/app/shared/components/fibonacci-clock/fibonacci-clock.component.spec.ts
+++ src/app/shared/components/fibonacci-clock/fibonacci-clock.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FibonacciClockComponent } from './fibonacci-clock.component';
 import { ColorPipe } from '@pipes';
+import { Color } from '@enums';
 
 describe('FibonacciClockComponent', () => {
     let component: FibonacciClockComponent;
@@ -19,5 +20,26 @@ describe('FibonacciClockComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should display updated title after detectChanges', () => {
+        component.fibonacciArray = [Color.RED, Color.BLUE, Color.GREEN, Color.WHITE, Color.RED];
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(getComputedStyle(compiled.querySelector('.rect-1')).getPropertyValue('background-color')).toContain(
+            'rgb(255, 0, 0)',
+        );
+        expect(getComputedStyle(compiled.querySelector('.rect-2')).getPropertyValue('background-color')).toContain(
+            'rgb(0, 0, 255)',
+        );
+        expect(getComputedStyle(compiled.querySelector('.rect-3')).getPropertyValue('background-color')).toContain(
+            'rgb(0, 128, 0)',
+        );
+        expect(getComputedStyle(compiled.querySelector('.rect-4')).getPropertyValue('background-color')).toContain(
+            'rgb(255, 255, 255)',
+        );
+        expect(getComputedStyle(compiled.querySelector('.rect-5')).getPropertyValue('background-color')).toContain(
+            'rgb(255, 0, 0)',
+        );
     });
 });
